b4a12b766d9dc2f78ac9dd9c0ef17853687fd52b
Brian C. Albrecht <brian@briancalbrecht.com>
2019-02-14 20:19:54 -0500

    added tikz

---
+++ docs/slides/convert_PDF_to_PNG.R
@@ -0,0 +1,29 @@
+
+# convert the charts from tikz (a pdf)
+# into single `png`s, one per page
+
+
+library(magick)
+library(pdftools)
+
+
+path <- "tikz_charts/evolution.pdf"
+
+nr_pages <-  pdftools::pdf_info(path)$pages  
+
+for (i in seq_along(1:nr_pages)) {
+    
+    file_name <- paste0("images/evolution/tikz_", LETTERS[i], ".png")
+
+    image_read_pdf(path, pages = i) %>% 
+        image_trim() %>%
+        image_convert("png") %>% 
+        image_crop("2550x2350") %>% 
+        image_transparent("white") %>% 
+        image_write(path = file_name, format = "png")
+    
+
+}
+
+
+
